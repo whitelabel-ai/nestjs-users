252b343b35a5054f29d6ddef54b205552eb0e87f
MedievalScript <medieval.script.2022@gmail.com>
2024-11-27 18:01:01 -0300

    feat: remove unnecesary comment

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { User, UserType } from '@prisma/client';
 
-// TODO: update DTO according to the register one. And check why is not working the class validator
 export class CreateUserDto {
   @ApiProperty({ required: true })
   email: string;
